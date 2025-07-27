cc0e18b8aeb4571959b32ed99d1922052279309a
sirhCC <ccharles199220@gmail.com>
2025-07-27 00:17:50 -0400

    fixed test error

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,10 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
   setupFilesAfterEnv: ['<rootDir>/src/__tests__/setup.ts'],
+  testMatch: [
+    '<rootDir>/src/__tests__/**/*.test.{ts,tsx}',
+  ],
+  testPathIgnorePatterns: [
+    '<rootDir>/src/__tests__/setup.ts'
+  ]
 };
